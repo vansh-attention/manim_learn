4dd8d1a81652ab806dbb0a83f291accfac15c6bc
vansh-attenion <vkhosla6293@gmail.com>
2025-07-03 01:02:54 +0530

    2.7.25

--- manim_learn.py
+++ manim_learn.py
@@ -1,5 +1,8 @@
 from manim import *
 class NameOfAnimation(Scene):
     def construct(self):
-        box = Rectangle(stroke_color = GREEN_B , stroke_opacity = 0.5)
-        fill_color = RED_B
+        box = Rectangle(stroke_color = GREEN_B , stroke_opacity = 0.5 , 
+        fill_color = RED_B , fill_opacity = 0.5 , height = 1 , width = 1 )
+
+        self.add(box)
+        self.play(box.animate.shift(RIGHT * 2), run_time = 2)
